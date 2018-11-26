856b5a22a3259488e2fd0c66ba82b6eb431bbb09
githubvtsan <Vi-hung@hotmail.com>
2018-11-26 01:11:13 +0100

    updated

---
+++ Philosophy of engineering science and design.mm
@@ -0,0 +1,120 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Philosophy of engineering science and design" FOLDED="false" ID="ID_1854343533" CREATED="1541526758412" MODIFIED="1541527268728" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="emperical facts" POSITION="right" ID="ID_544802231" CREATED="1541526795695" MODIFIED="1541526872885">
+<edge COLOR="#ff0000"/>
+<node TEXT="observable" ID="ID_12848332" CREATED="1541526872895" MODIFIED="1541526897765"/>
+<node TEXT="measured\direct observation" ID="ID_1268215987" CREATED="1541527171021" MODIFIED="1541527197196"/>
+<node TEXT="temperature of water is 5 degrees" ID="ID_712706778" CREATED="1541527216073" MODIFIED="1541527256290"/>
+</node>
+<node TEXT="claims" POSITION="left" ID="ID_785091231" CREATED="1541526910366" MODIFIED="1541527297022">
+<edge COLOR="#0000ff"/>
+<node TEXT="factual\descriptive" ID="ID_377294665" CREATED="1541526934123" MODIFIED="1541527014941">
+<node TEXT="true or false" ID="ID_571041265" CREATED="1541527017189" MODIFIED="1541527027182"/>
+<node TEXT="state of affairs" ID="ID_1919729813" CREATED="1541527302550" MODIFIED="1541527318862">
+<node TEXT="has been" ID="ID_1820017812" CREATED="1541527318862" MODIFIED="1541527327620"/>
+<node TEXT="would be" ID="ID_789052514" CREATED="1541527330083" MODIFIED="1541527345160"/>
+<node TEXT="is" ID="ID_1803229523" CREATED="1541527345629" MODIFIED="1541527347864"/>
+<node TEXT="might be" ID="ID_716008995" CREATED="1541527348254" MODIFIED="1541527356706"/>
+</node>
+<node TEXT="world decides" ID="ID_37445347" CREATED="1541527472105" MODIFIED="1541527497196"/>
+</node>
+<node TEXT="normative" ID="ID_172564443" CREATED="1541526997327" MODIFIED="1541527033773">
+<node TEXT="desirable" ID="ID_1724230223" CREATED="1541527038077" MODIFIED="1541527383243">
+<node TEXT="good\wrong" ID="ID_1210230646" CREATED="1541527383243" MODIFIED="1541527390208"/>
+<node TEXT="agree\disagree" ID="ID_1566067486" CREATED="1541527390614" MODIFIED="1541527410178"/>
+</node>
+<node TEXT="action-oriented" ID="ID_1996736642" CREATED="1541527511604" MODIFIED="1541527526718">
+<node TEXT="implicit recommendation" ID="ID_1029427935" CREATED="1541527532609" MODIFIED="1541527555056"/>
+</node>
+<node TEXT="different judgment possible" ID="ID_1897567668" CREATED="1541527562522" MODIFIED="1541527572411"/>
+<node TEXT="objective" ID="ID_1239351448" CREATED="1541527705257" MODIFIED="1541527848563">
+<node TEXT="valid for everyone" ID="ID_1019602391" CREATED="1541527719758" MODIFIED="1541527726012">
+<node TEXT="one should not kill" ID="ID_1575703197" CREATED="1541527731637" MODIFIED="1541527737700"/>
+</node>
+<node TEXT="open to public" ID="ID_992381125" CREATED="1541527836892" MODIFIED="1541527853766"/>
+</node>
+<node TEXT="not subjective" ID="ID_1997771051" CREATED="1541527753580" MODIFIED="1541527763171">
+<node TEXT="subjective" ID="ID_1163576124" CREATED="1541527765859" MODIFIED="1541527771211">
+<node TEXT="one particular person felt or judge" ID="ID_351857182" CREATED="1541527771226" MODIFIED="1541527800801"/>
+<node TEXT="only valid for a person" ID="ID_1470461080" CREATED="1541527801348" MODIFIED="1541527811644"/>
+</node>
+</node>
+<node TEXT="not equal to vagueness" ID="ID_1343699511" CREATED="1541527912837" MODIFIED="1541527920396">
+<node TEXT="crime" ID="ID_1379237795" CREATED="1541527936035" MODIFIED="1541527939364">
+<node TEXT="vague" ID="ID_57906277" CREATED="1541527939364" MODIFIED="1541527943645">
+<node TEXT="but open to investigation" ID="ID_1987672340" CREATED="1541527943645" MODIFIED="1541527950910"/>
+<node TEXT="out there in the world" ID="ID_690203308" CREATED="1541527951207" MODIFIED="1541528012738"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="" POSITION="right" ID="ID_1521544050" CREATED="1541527163477" MODIFIED="1541527163478">
+<edge COLOR="#00ff00"/>
+</node>
+</node>
+</map>
